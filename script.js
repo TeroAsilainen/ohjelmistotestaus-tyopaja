a0cccd899d283d08081c0968ad8242b0073502e7
TeroAsilainen <tero_asilainen@hotmail.com>
2025-07-15 13:11:14 +0300

    laskutoiminnot toimii

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-
-
-
-
 //summa
 document.getElementById('summa').addEventListener('click', () => {
     const a = document.getElementById('a').value;
@@ -11,6 +7,48 @@ document.getElementById('summa').addEventListener('click', () => {
     } else{
         const summa = parseFloat(a) + parseFloat(b);
     document.getElementById('tulos').innerHTML = summa;
+    }
+    
+}
+)
+
+//erotus
+document.getElementById('erotus').addEventListener('click', () => {
+    const a = document.getElementById('a').value;
+    const b = document.getElementById('b').value;
+    if (isNaN(a) || isNaN(b)){
+        document.getElementById('tulos').innerHTML = 'Virhe!';
+    } else{
+        const erotus = parseFloat(a) - parseFloat(b);
+    document.getElementById('tulos').innerHTML = erotus;
+    }
+    
+}
+)
+
+//tulo
+document.getElementById('tulo').addEventListener('click', () => {
+    const a = document.getElementById('a').value;
+    const b = document.getElementById('b').value;
+    if (isNaN(a) || isNaN(b)){
+        document.getElementById('tulos').innerHTML = 'Virhe!';
+    } else{
+        const tulo = parseFloat(a) * parseFloat(b);
+    document.getElementById('tulos').innerHTML = tulo;
+    }
+    
+}
+)
+
+//osamäärä
+document.getElementById('osamaara').addEventListener('click', () => {
+    const a = document.getElementById('a').value;
+    const b = document.getElementById('b').value;
+    if (isNaN(a) || isNaN(b)){
+        document.getElementById('tulos').innerHTML = 'Virhe!';
+    } else{
+        const osamaara = parseFloat(a) / parseFloat(b);
+    document.getElementById('tulos').innerHTML = osamaara;
     }
     
 }
